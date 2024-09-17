2ea0912fcc89771c212fe5fc0443de3e16f9fdaf
Vinicius Cardoso <vinic8670@gmail.com>
2024-09-17 20:36:02 -0300

    fix: format data

--- assets/js/main.js
+++ assets/js/main.js
@@ -176,8 +176,18 @@ document.getElementById("close-edit-modal").addEventListener("click", () => {
 });
 
 function saveData() {
+  const now = new Date();
+  const formattedDate = `${now.getFullYear()}-${(now.getMonth() + 1)
+    .toString()
+    .padStart(2, "0")}-${now.getDate().toString().padStart(2, "0")}T${now
+    .getHours()
+    .toString()
+    .padStart(2, "0")}:${now.getMinutes().toString().padStart(2, "0")}:${now
+    .getSeconds()
+    .toString()
+    .padStart(2, "0")}`;
   const gameData = {
-    date: new Date().toLocaleString(),
+    date: formattedDate,
     seconds,
     isTimerRunning,
     team1: {
